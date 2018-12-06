2f0a81576f6d51591acd3436d188f8f9c627f6fc
jochen@homeland <no-go@localhost>
2018-12-06 23:52:15 +0100

    keyboard seperate project

--- yidkey/app/src/main/java/click/dummer/yidkey/Application.java
+++
@@ -1,9 +0,0 @@
-package click.dummer.yidkey;
-
-public final class Application extends android.app.Application {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        //FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/c64pro_mono.ttf");
-    }
-}
